2a7bbe79005554a6d26435f95c1b2e009babe925
Mahek Roy <agentroykvsil@gmail.com>
2025-10-06 16:54:34 +0530

    Update destruct.js

--- JS_advanced/destruct.js
+++ JS_advanced/destruct.js
@@ -16,5 +16,35 @@ const obj = { name: "Mahek", age: 35, daughter: "Prokrity" };
 /*normally we have to write "obj.name, obj.age etc..." 👆 to access each of this values in a object,
 so if we dont want this then here comes the destructuring*/
 const { daughter } = obj;
+console.log(daughter);  // prokrity
 /* 👆 so what it will do, it create a variable named "daughter" and then it 
 will take out that value from "obj"*/
+
+// >Using Default Values:
+
+const fruits = ["apple"];
+
+const [first, second = "banana"] = fruits;   /*👈If the second value doesn’t exist, it takes the default.*/
+console.log(first, second); // apple banana
+
+// >Array Destructuring with Rest Operator:
+
+const scores = [85, 90, 78, 92];
+
+const [top1, top2, ...others] = scores;
+
+console.log(top1, top2);   // 85 90
+console.log(others.join(" "));   // 78 92
+
+// >Object Destructuring:
+
+const user = {
+  name: "Mahek",
+  age: 25,
+  city: "Silchar"
+};
+
+const { name, age } = user;
+console.log(name, age); // Mahek 25
+
+// >
