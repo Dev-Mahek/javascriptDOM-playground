078bb406774d4071c52b4e2bc16dc779b1c7ae71
Mahek Roy <agentroykvsil@gmail.com>
2025-10-06 14:13:46 +0530

    Update defo_para.js

--- JS_advanced/defo_para.js
+++ JS_advanced/defo_para.js
@@ -8,3 +8,31 @@ var fune = (prem = 0) => {
 fune(22); /*👈value give here it will show 22 as print*/
 fune();
 /* 👆 if we want that when there is no value given and we dont want it to show "undefine" then it must show "0"*/
+
+var greet = (name = "guest") => {
+  console.log(`Welcome back user: ${name}!`);
+};
+
+greet("Mahek");  // Output: Hello, Roy! 
+greet();        // Output: Hello, Guest!
+
+// >Multiple Default Parameters:
+
+let intro = (name = "Unknown", country = "Unknown") => {    /*👈Each parameter has its own default value.*/
+  return `Hii i am ${name}, and I am form ${country}.`;
+};
+
+console.log(intro("Mahek", "India"));
+console.log(intro());
+
+const power = (base, exponent = 2) => base ** exponent;
+console.log(power(5));    // Output: 25 (defaults to square)
+console.log(power(5, 3)); // Output: 125
+
+// >Default Parameter with Expression:
+ function getDiscount(price, rate = price > 100 ? 0.1 : 0.05) {
+  return price - price * rate;
+}
+
+console.log(getDiscount(150)); // 10% discount → Output: 135
+console.log(getDiscount(80));  // 5% discount → Output: 76
