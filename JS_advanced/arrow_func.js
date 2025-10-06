ec2a6d6123766a668ac4d93368ef0530fb14c649
Mahek Roy <agentroykvsil@gmail.com>
2025-10-06 09:58:32 +0530

    Update arrow_func.js

--- JS_advanced/arrow_func.js
+++ JS_advanced/arrow_func.js
@@ -2,16 +2,42 @@
 
 // >Basic Fat Arrow Fun:-
 var fun = () => {};
-// >Fat Arrow Fun with one parameter:-(if we want then we can remove those fat brackets() in case of one value marameter)
+
+// >Fat Arrow Fun with one parameter:-(if we want then we can remove those fat brackets() in case of one value parameter)
 var funb = (parame) => {
   console.log(parame);
 };
 funb(12);
+
+var sqr = (num) => {
+  console.log(num * num);
+};
+
+sqr(25);
+
 // for 2 value we have to add another parameter as well as we have to add this brackets () now:
 var funbb = (parame, huie) => {};
 funb(12, 13);
+
+var add = (num1, num2) => {
+  console.log(num1 + num2);
+};
+
+add(2, 2);
+
 // >Fat Arrow Fun implicit return:-(after arrow whatever is written will return)
 var fund = () => 12;
 console.log(
   fund()
-); /*the value "12"☝ is now in "fund()" so we will console.log it to see the value*/
+); /*the value "12">👆 is now in "fund()" so we will console.log it to see the value*/
+
+const greet = (name) => `Hii ${name}, how are you`;
+
+console.log(greet("Mahek"));
+
+// >Fat Arrow Fun with explicit return
+const multi = (x, y) => {
+  return `the multiplication is ${x * y}`;
+};
+
+console.log(multi(2, 4));
