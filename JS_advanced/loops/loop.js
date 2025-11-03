3117b015ed69bf362c65764ac4d1bddb7229d98d
Mahek Roy <agentroykvsil@gmail.com>
2025-11-03 23:06:20 +0530

    Create loop.js

---
+++ JS_advanced/loops/loop.js
@@ -0,0 +1,17 @@
+// ---------to loop in arrays[]-------------
+
+var a = [1, 2, 3, 4, 5, 6];
+
+a.forEach(function (val) {
+  //   console.log(val + 2);
+});
+
+// ---------to loop in objects{}------------
+
+var b = { name: "Mahek", age: "24", partner: "still waiting😅" };
+
+for (var key in b) {
+  //   console.log(key);
+  //   console.log(b[key]);
+  //   console.log(key, b[key]);
+}
